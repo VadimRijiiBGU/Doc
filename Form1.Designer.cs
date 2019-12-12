0fbad7012c48f50b29d71fb53da6139c3bf17b13
VadimRijiiBGU <37809708+VadimRijiiBGU@users.noreply.github.com>
2019-12-12 22:20:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DocForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.generateCountTextBox = new System.Windows.Forms.TextBox();
+            this.createDoc = new System.Windows.Forms.Button();
+            this.openDoc = new System.Windows.Forms.Button();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.generateCountLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AttestateNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DisciplineName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.generate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generateCountTextBox
+            // 
+            this.generateCountTextBox.Location = new System.Drawing.Point(174, 241);
+            this.generateCountTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.generateCountTextBox.Name = "generateCountTextBox";
+            this.generateCountTextBox.Size = new System.Drawing.Size(200, 22);
+            this.generateCountTextBox.TabIndex = 0;
+            // 
+            // createDoc
+            // 
+            this.createDoc.Location = new System.Drawing.Point(14, 458);
+            this.createDoc.Margin = new System.Windows.Forms.Padding(4);
+            this.createDoc.Name = "createDoc";
+            this.createDoc.Size = new System.Drawing.Size(183, 48);
+            this.createDoc.TabIndex = 2;
+            this.createDoc.Text = "Create doc";
+            this.createDoc.UseVisualStyleBackColor = true;
+            this.createDoc.Click += new System.EventHandler(this.createDoc_Click);
+            // 
+            // openDoc
+            // 
+            this.openDoc.Location = new System.Drawing.Point(13, 14);
+            this.openDoc.Margin = new System.Windows.Forms.Padding(4);
+            this.openDoc.Name = "openDoc";
+            this.openDoc.Size = new System.Drawing.Size(183, 48);
+            this.openDoc.TabIndex = 5;
+            this.openDoc.Text = "Open doc";
+            this.openDoc.UseVisualStyleBackColor = true;
+            this.openDoc.Click += new System.EventHandler(this.openDoc_Click);
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(174, 111);
+            this.numberTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(200, 22);
+            this.numberTextBox.TabIndex = 8;
+            this.numberTextBox.TextChanged += new System.EventHandler(this.numberTextBox_TextChanged);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(174, 190);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(200, 22);
+            this.nameTextBox.TabIndex = 10;
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(13, 116);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(58, 17);
+            this.numberLabel.TabIndex = 11;
+            this.numberLabel.Text = "Number";
+            this.numberLabel.Click += new System.EventHandler(this.numberLabel_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(174, 153);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.Value = new System.DateTime(2019, 11, 14, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Location = new System.Drawing.Point(12, 158);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(38, 17);
+            this.dateLabel.TabIndex = 13;
+            this.dateLabel.Text = "Date";
+            this.dateLabel.Click += new System.EventHandler(this.dateLabel_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(11, 190);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 17);
+            this.nameLabel.TabIndex = 14;
+            this.nameLabel.Text = "Name";
+            this.nameLabel.Click += new System.EventHandler(this.nameLabel_Click);
+            // 
+            // generateCountLabel
+            // 
+            this.generateCountLabel.AutoSize = true;
+            this.generateCountLabel.Location = new System.Drawing.Point(12, 244);
+            this.generateCountLabel.Name = "generateCountLabel";
+            this.generateCountLabel.Size = new System.Drawing.Size(107, 17);
+            this.generateCountLabel.TabIndex = 18;
+            this.generateCountLabel.Text = "Generate count";
+            this.generateCountLabel.Click += new System.EventHandler(this.generateCountLabel_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.AttestateNum,
+            this.DisciplineName,
+            this.Result,
+            this.Date});
+            this.dataGridView1.Location = new System.Drawing.Point(399, 14);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(732, 505);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnWidthChanged);
+            this.dataGridView1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView1_Scroll);
+            // 
+            // AttestateNum
+            // 
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.AttestateNum.DefaultCellStyle = dataGridViewCellStyle5;
+            this.AttestateNum.Frozen = true;
+            this.AttestateNum.HeaderText = "№ дисциплины";
+            this.AttestateNum.MinimumWidth = 120;
+            this.AttestateNum.Name = "AttestateNum";
+            this.AttestateNum.Width = 120;
+            // 
+            // DisciplineName
+            // 
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.DisciplineName.DefaultCellStyle = dataGridViewCellStyle6;
+            this.DisciplineName.Frozen = true;
+            this.DisciplineName.HeaderText = "Название дисциплины";
+            this.DisciplineName.MinimumWidth = 6;
+            this.DisciplineName.Name = "DisciplineName";
+            this.DisciplineName.Width = 125;
+            // 
+            // Result
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.Result.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Result.Frozen = true;
+            this.Result.HeaderText = "Результат";
+            this.Result.MinimumWidth = 6;
+            this.Result.Name = "Result";
+            this.Result.Width = 125;
+            // 
+            // Date
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Date.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Date.Frozen = true;
+            this.Date.HeaderText = "Дата";
+            this.Date.MinimumWidth = 6;
+            this.Date.Name = "Date";
+            this.Date.Width = 125;
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(76, 298);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(185, 49);
+            this.generate.TabIndex = 21;
+            this.generate.Text = "Generate ";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // DocForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 519);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.generateCountLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.openDoc);
+            this.Controls.Add(this.createDoc);
+            this.Controls.Add(this.generateCountTextBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "DocForm";
+            this.Text = "VadimRijiiBGU";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox generateCountTextBox;
+        private System.Windows.Forms.Button createDoc;
+        private System.Windows.Forms.Button openDoc;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label generateCountLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AttestateNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DisciplineName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApp1
+{
+    public partial class DocForm : Form
+    {
+        Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+        Document doc;
+
+        public struct My_Row
+        {
+            public string name;
+            public string number;
+            public DateTime date;
+            public int result;
+        }
+
+        DateTimePicker dtp = new DateTimePicker();
+        System.Drawing.Rectangle _Rectangle;
+
+        List<My_Row> rows_list = new List<My_Row>();
+
+        public DocForm()
+        {
+            InitializeComponent();
+            dataGridView1.Controls.Add(dtp);
+            dtp.Visible = false;
+            dtp.Format = DateTimePickerFormat.Custom;
+            dtp.TextChanged += new EventHandler(dtp_TextChange);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openDoc_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = @"E:\Study\BankBusiness";
+                openFileDialog.Filter = "Office Files|*.docx;*.dotx";
+                //  openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    doc = app.Documents.Add(filePath);
+
+                }
+            }
+
+        }
+
+        private void createDoc_Click(object sender, EventArgs e)
+        {
+            // ConsoleManager.Show();
+            Table table = doc.Tables[1];
+            My_Row[] my_Rows;
+
+
+
+            Console.WriteLine(dataGridView1.Rows.Count);
+            for (int rows = 0; rows < dataGridView1.Rows.Count - 1; rows++)
+            {
+                My_Row row_adding;
+                row_adding.number = dataGridView1.Rows[rows].Cells["AttestateNum"].Value.ToString();
+                row_adding.name = dataGridView1.Rows[rows].Cells["DisciplineName"].Value.ToString();
+                row_adding.result = Int32.Parse(dataGridView1.Rows[rows].Cells["Result"].Value.ToString());
+                row_adding.date = DateTime.Parse(dataGridView1.Rows[rows].Cells["Date"].Value.ToString());
+                rows_list.Add(row_adding);
+            }
+            for (int j = 0; j < rows_list.Count - 1; j++)
+            {
+                table.Rows.Add(table.Rows[table.Rows.Count]);
+            }
+
+            my_Rows = rows_list.ToArray();
+
+
+
+            int i = -1;
+
+            foreach (Row row in table.Rows)
+            {
+                foreach (Cell cell in row.Cells)
+                {
+                    Console.WriteLine(i);
+                    if (cell.RowIndex != 1)
+                    {
+                        switch (cell.ColumnIndex)
+                        {
+                            case 1:
+                                {
+                                    cell.Range.Text = my_Rows[i].number;
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    cell.Range.Text = my_Rows[i].name;
+
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    cell.Range.Text = my_Rows[i].result.ToString();
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    cell.Range.Text = my_Rows[i].date.ToString().Substring(0, 10);
+                                    break;
+                                }
+                        }
+                        cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                    }
+
+                }
+                i++;
+            }
+            foreach (Microsoft.Office.Interop.Word.FormField field in doc.FormFields)
+            {
+                switch (field.Name)
+                {
+                    case "number":
+                        field.Range.Text = numberTextBox.Text;
+                        break;
+                            
+                    case "date":
+                        field.Range.Text = dateTimePicker1.Value.ToString().Substring(0,10);
+                        break;
+
+                    case "personName":
+                        field.Range.Text = nameTextBox.Text;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        
+            app.Visible = true;
+    
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            doc.Save();
+            doc.Close();
+            app.Quit();
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               // app.Quit();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            switch (dataGridView1.Columns[e.ColumnIndex].Name)
+            {
+                case "Date":
+                    _Rectangle = dataGridView1.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, true);
+                    dtp.Size = new Size(_Rectangle.Width, _Rectangle.Height);
+                    dtp.Location = new System.Drawing.Point(_Rectangle.X, _Rectangle.Y);
+                    dtp.Visible = true;
+
+                    break;
+            }
+        }
+
+        private void dtp_TextChange(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell.Value = dtp.Text.ToString();
+        }
+
+        private void dataGridView1_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
+        {
+            dtp.Visible = false;
+        }
+
+        private void dataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            dtp.Visible = false;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            int counter = -1;
+            if (!Int32.TryParse(generateCountTextBox.Text, out counter))
+            {
+
+            }
+
+            Console.WriteLine(counter);
+
+            dataGridView1.RowCount = counter + 1;
+
+            Random random = new Random();
+            for (int j = 0; j < counter; j++)
+            {
+                int res = random.Next(0, counter - 1);
+                int name_int = random.Next(0, counter - 1);
+                DateTime start = new DateTime(1995, 1, 1);
+                int range = (DateTime.Today - start).Days;
+                int price = random.Next(1, 100000);
+
+                dataGridView1.Rows[j].Cells[0].Value = random.Next(0, counter - 1);
+                dataGridView1.Rows[j].Cells[1].Value = "name" + name_int.ToString();
+                dataGridView1.Rows[j].Cells[2].Value = random.Next(1, 10);
+                dataGridView1.Rows[j].Cells[3].Value = start.AddDays(random.Next(range)).ToString().Substring(0, 10);
+
+            }
+        }
+
+        private void numberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void generateCountLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
